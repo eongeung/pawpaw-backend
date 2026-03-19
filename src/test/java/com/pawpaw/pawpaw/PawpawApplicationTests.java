be01b4f943c1fce2a72c457a105d4532fa7bb5bb
eongeung <iyoungheuni@gmail.com>
2026-03-19 18:30:29 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/com/pawpaw/pawpaw/PawpawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawpaw.pawpaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawpawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
