c2817385f6644b2ad6a48e381ab0fa787dece2ef
eongeung <iyoungheuni@gmail.com>
2026-03-22 22:22:12 +0900

    feat: 동물병원 검색 및 리뷰 기능 구현

---
+++ src/main/java/com/pawpaw/pawpaw/domain/hospital/dto/NaverSearchResponse.java
@@ -0,0 +1,22 @@
+package com.pawpaw.pawpaw.domain.hospital.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class NaverSearchResponse {
+    private List<Item> items;
+
+    @Getter
+    @Setter
+    public static class Item {
+        private String title;
+        private String address;
+        private String roadAddress;
+        private String telephone;
+        private String mapx;
+        private String mapy;
+    }
+}
